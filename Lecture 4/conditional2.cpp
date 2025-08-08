b2cfd0ccf59140d0be0d9407c1253e49f6e39507
ubaid bhat <ubaidyaqoob779@gmail.com>
2025-08-08 15:13:20 +0530

    done

---
+++ Lecture 4/conditional2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin>>x;
+    cin>>y;
+    if(x>y){
+       // cout<<"x is greater than y";
+       if(x==y){
+        
+       }
+    }
+    else{
+        cout<<"y is greater or equal to x";
+    
+    }
+}
---
+++ Lecture 4/sub.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a = 20;
+    float b = 5;
+    float sub = a - b;
+    cout<<sub<<endl;
+
+}
